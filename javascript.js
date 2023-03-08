b08722ec83d53f5873c2571033123a42540a6f29
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-03-08 12:36:34 -0500

    added git to files
    calculator operation function calls functions for add subtract,
    multiply or divide then returns value;
    
    created simple interface with buttons that don't yet connect.

---
+++ javascript.js
@@ -0,0 +1,47 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+     return num1 * num2;
+}
+
+function divide(num1, num2){
+      return num1 / num2;
+}
+
+
+function operate(num1, operator, num2){
+    switch(operator){
+   case "+":
+     let addition = add(num1, num2);
+     return addition
+   case "-":
+    let subtraction = subtract(num1,num2);
+    return subtraction; 
+   case "*":
+    let multiplication = multiply(num1, num2)
+    return multiplication;
+   case "/":
+    let division = divide(num1,num2);
+    return division; 
+    }
+
+}
+
+console.log(operate(9, "+", 4));
+console.log(operate(9, "-", 4));
+console.log(operate(9, "*", 4));
+console.log(operate(9, "/", 4));
+
+
+// console.log(add(2,8));
+// console.log(subtract(2,8));
+// console.log(multiply(2,8));
+// console.log(divide(2,8));
+// console.log(divide(100,5));
+
